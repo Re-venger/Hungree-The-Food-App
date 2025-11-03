16b341bce6cacf47fa55ad85f681bb090b53cac7
Yogesh <yogeshkk1818@gmail.com>
2025-11-03 20:15:40 +0530

    Major Changes Done

--- metro.config.js
+++ metro.config.js
@@ -1,4 +1,13 @@
-const { withNativeWind } = require('nativewind/metro');
-const config = {}
- 
-module.exports = withNativeWind(config, { input: './app/globals.css' })
+// metro.config.js
+const { getDefaultConfig } = require("@expo/metro-config");
+const { withNativeWind } = require("nativewind/dist/metro/index.js"); // correct require for NativeWind
+
+// Load the default Expo Metro config
+const defaultConfig = getDefaultConfig(__dirname);
+
+// Apply NativeWind configuration
+const config = withNativeWind(defaultConfig, {
+  input: "./app/globals.css", // your Tailwind globals
+});
+
+module.exports = config;
